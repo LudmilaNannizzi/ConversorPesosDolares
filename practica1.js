25213e57680484ed6c0062e4816b4841b8f3e77f
Ludmila <nannizziludmilaa@gmail.com>
2021-08-26 14:39:39 -0300

    Conversor de pesos a dolares

---
+++ practica1.js
@@ -0,0 +1,22 @@
+function cambiarDolares() {
+    let dolarHoy = 102.5
+    
+    let ingresoPesos=document.getElementById("pesos").value
+    resultado=   ingresoPesos / dolarHoy ; 
+
+    let impuestoPais = ingresoPesos * 0.3 ;
+    let retencionGanancias = ingresoPesos * 0.35;
+
+    let valorReal;
+    valorReal = parseInt(ingresoPesos) + parseInt(impuestoPais) + parseInt(retencionGanancias);
+
+   
+    if(resultado<=0){
+        alert('El monto ingresado es incorrecto')
+    }
+        else{
+        alert('$'+ ingresoPesos+ ' pesos argentinos son US$'+resultado + '. El impuesto país es de $' + impuestoPais + ' y las retenciones equivalen a $'+ retencionGanancias+' pesos argentinos. El total a pagar es de $'+ valorReal)
+    }
+
+
+}
